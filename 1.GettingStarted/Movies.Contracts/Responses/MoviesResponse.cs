f9f9341494762761d232a7f5a1996ff163ba066e
Nathan <nathan.hemmings@proton.me>
2026-02-10 08:44:17 -0600

    Defining the movie API contracts and temporary database.

---
+++ 1.GettingStarted/Movies.Contracts/Responses/MoviesResponse.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Movies.Contracts.Responses;
+
+public class MoviesResponse
+{
+    public required IEnumerable<MovieResponse> Items { get; init; } = Enumerable.Empty<MovieResponse>();
+}
